4ccaad4865e9b0c3bff9ed7b05ff079c6151c421
dhirajhanagal007-eng <dhirajhanagal007@gmail.com>
2026-09-16 20:45:13 +0530

    Update lab_program_4.py

--- lab_program_4.py
+++ lab_program_4.py
@@ -3,6 +3,14 @@ num2 = float(input("Enter second number: "))
 
 addition = num1 + num2
 subtraction = num1 - num2
+multiplication = num1 * num2
 
 print("Addition:", addition)
 print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+
+if num2 != 0:
+    division = num1 / num2
+    print("Division:", division)
+else:
+    print("Division: Cannot divide by zero")
